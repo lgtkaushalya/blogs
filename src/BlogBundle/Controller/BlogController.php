0088a833655410b8a6bcbb07080583d026450d5b
Thilanka <lgtkaushalya@gmail.com>
2015-11-18 06:49:49 +0530

    Added list view

--- src/BlogBundle/Controller/BlogController.php
+++ src/BlogBundle/Controller/BlogController.php
@@ -2,8 +2,9 @@
 namespace BlogBundle\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
-class BlogController {
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+class BlogController extends Controller {
   public function listAction() {
-    return new Response("Welcome to Blog List");
+    return $this->render('BlogBundle:Blog:list.html.twig');
   }
 }
